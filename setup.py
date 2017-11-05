cb9495d3a82811125a3bf50815a3eed5d83f71fe
Iggy <iggy@theiggy.com>
2017-11-05 13:49:46 -0800

    shutup pylint/pyflakes

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Distutils setup file."""
 
 from distutils.core import setup
 
@@ -8,5 +9,4 @@ setup(name='salt_grain_gce',
       author='Iggy',
       author_email='iggy@theiggy.com',
       url='https://github.com/saltstack-contrib/grains-gce',
-      py_modules=['_grains/gce'],
-      )
+      py_modules=['_grains/gce'])
